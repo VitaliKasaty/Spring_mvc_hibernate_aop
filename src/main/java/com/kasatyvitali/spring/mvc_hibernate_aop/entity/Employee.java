3c2df911676c699f62fe3fe09be44773e60525b9
Vitali Kasaty <vitalik007007@gmail.com>
2022-06-13 17:37:18 +0300

    create project

---
+++ src/main/java/com/kasatyvitali/spring/mvc_hibernate_aop/entity/Employee.java
@@ -0,0 +1,10 @@
+package com.kasatyvitali.spring.mvc_hibernate_aop.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+}
